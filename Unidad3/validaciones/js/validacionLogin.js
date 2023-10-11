65e75f99f4286e4ec12d403162e39ca5a7962e68
♡Melissa♡ <21CG0138@itsncg.edu.mx>
2023-10-11 14:21:17 -0600

    Trabajo en js con login y alertas

---
+++ Unidad3/validaciones/js/validacionLogin.js
@@ -0,0 +1,36 @@
+window.onload=()=>{
+    //detectar cuando pagina inicio esten cargandos en paginas el codigo scritp se haya cargado 
+    var btn=document.getElementById("btnLogin")
+
+    let txtE= document.getElementById('txtEmail')//llamar por su id
+    let txtP=document.querySelector("#txtPassword")//llamamos objetos que tienen clase
+    let divA=document.querySelector("#alert")//el nombre tiene que se rigual que el js el de los parentesis
+    let divAS=document.querySelector("#alertSuccess")
+
+    btn.addEventListener('click',(evt)=>{
+        evt.preventDefault()
+
+       
+        if(txtE.value== "" || txtP.value ==""){
+            divA.innerHTML='Error: <br> <small>favor de llenar los campos</small>'//estoy cambiando conetido o html dentro de la equieta
+        divA.style.display="block"//style:para obtner estilo display accedo a propiedad y block par que se vea
+        }else{
+        if(txtP.value.length<5){
+            divA.innerHTML='Error: <br> <small>password debe ser mayor de 5</small>'
+            divA.style.display="block"
+        }else{
+            divAS.style.display="block" 
+        }
+        }
+    })
+
+    txtE.addEventListener('focus',(evt)=>{
+        divA.style.display='none'
+        divAS.style.display='none'
+    })
+    txtP.addEventListener('focus',(evt)=>{
+        divA.style.display='none'
+        divAS.style.display='none'
+            })
+  
+}
